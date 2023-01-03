0595a6406d11cf7f4783ee6ed2589a4752e16548
Tucker Tibbetts <tucker.tibbetts@afs.com>
2023-01-03 15:27:28 -0500

    fix errors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,14 +15,14 @@ set -x PIP_TRUSTED_HOST nma-dev.novetta.com
 
 source ~/.config/fish/dircolors.fish
 
-set KUBE_EDITOR /mnt/c/Program\ Files/Sublime\ Text\ 3/subl.exe
-set HELM_EXPERIMENTAL_OCI 1
-
-nvm use 17
 . ~/.asdf/asdf.fish
 . ~/.asdf/completions/asdf.fish
 
-starship init fish | source
+if type -q starship
+    starship init fish | source
+end
 
-pokemon-colorscripts -r
+if type -q pokemon-colorscripts
+    pokemon-colorscripts -r
+end
 
